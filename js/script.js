85b90d0c41187d3d663f262866f57e041bb4e14f
yaroslav-dido <yarickdid090@gmail.com>
2022-12-01 00:53:39 +0200

    finished portfolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
 		menu = document.querySelector('.menu'),
-		overlay = document.querySelector('.menu__overlay')
+		overlay = document.querySelector('.menu__overlay'),
 		close = document.querySelector('.menu__block-close');
+		
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
@@ -14,3 +15,13 @@ close.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
+
+
+const procents = document.querySelectorAll('.procents'),
+		lines = document.querySelectorAll('.scales_scale div');
+
+procents.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
+
+
